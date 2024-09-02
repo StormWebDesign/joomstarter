9c8e3b7d41e88e97b98bd5edc216a74936eb7913
Russell English <russell.english@stormit.co.uk>
2024-09-02 15:39:49 +0100

    debugging

--- index.php
+++ index.php
@@ -67,10 +67,12 @@ $wa->useScript('template.joomstarter.scripts');
 
 // Conditionally load UIkit if enabled in the template parameters
 if ($this->params->get('uikit', 1)) {
+    echo '<!-- UIkit is enabled -->'; // Debugging line
     $wa->useStyle('template.uikit.css');
     $wa->useScript('template.uikit.js');
+} else {
+    echo '<!-- UIkit is disabled -->'; // Debugging line
 }
-
 
 //Set viewport meta tag for mobile responsiveness -- very important for scaling on mobile devices
 $this->setMetaData('viewport', 'width=device-width, initial-scale=1');
