53e63a6e5ce03c4c7ceac005f5a45623d4efadd1
Russell English <russell.english@stormit.co.uk>
2024-09-02 17:02:22 +0100

    Update script.php

--- script.php
+++ script.php
@@ -1,41 +1,26 @@
 <?php
 
-defined('_JEXEC') or die;
+defined ( '_JEXEC' ) or die;
 
-// The class name must match the "element" property in the extensions table of the database,
-// or the field "name" in the xml file EXACTLY or the script will not run.
+//the class name must match the "element" property in the extensions table of the database, or the field "name" in the xml file EXACTLY or the script will not run
 class joomstarterInstallerScript
 {
-    public function install($parent)
-    {
-       // Access the manifest object safely
-       $manifest = $parent->getManifest();
 
-       if ($manifest && $manifest->version) {
-           // Ensure the manifest version is available and convert to string
-           $version = (string) $manifest->version;
-           echo '<p>The template has been installed to the latest version: ' . htmlspecialchars($version) . '</p>';
-       } else {
-           echo '<p>Unable to determine the template version from the manifest.</p>';
-       }
+    public function install($parent) 
+    {
+        echo '<p> This is the install message called from the script.php file </p>';
     }
 
-    public function uninstall($parent)
+    public function uninstall($parent) 
     {
-        echo '<p>This is the uninstall message called from script.php.</p>';
+        echo '<p>This is the uninstall message called from script.php</p>';
     }
 
-    public function update($parent)
+    public function update($parent) 
     {
-        // Access the manifest object safely
-        $manifest = $parent->getManifest();
+        echo '<p>This is the update message called from script.php</p>';
 
-        if ($manifest && $manifest->version) {
-            // Ensure the manifest version is available and convert to string
-            $version = (string) $manifest->version;
-            echo '<p>The template has been updated to the latest version: ' . htmlspecialchars($version) . '</p>';
-        } else {
-            echo '<p>Unable to determine the template version from the manifest.</p>';
-        }
     }
+
+
 }
