44bc180111c2db0a3fcf91051708f6d3c0218aa1
Russell English <russell.english@stormit.co.uk>
2024-09-02 17:25:54 +0100

    still going...

--- index.php
+++ index.php
@@ -68,10 +68,8 @@ $wa->useScript('template.joomstarter.scripts');
 
 // Conditionally load UIkit if enabled in the template parameters
 
-if ($uikit == '1') {
-    $wa->useStyle('uikit.styles');
-    $wa->useScript('uikit.script');
-}
+$wa->useStyle('uikit.styles');
+$wa->useScript('uikit.script');
 
 //Set viewport meta tag for mobile responsiveness -- very important for scaling on mobile devices
 $this->setMetaData('viewport', 'width=device-width, initial-scale=1');
